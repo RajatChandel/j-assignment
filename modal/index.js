9a38565618913bea359aebd918b98b64b249919e
RajatChandel <rk.rajat500@gmail.com>
2018-08-06 14:37:34 +0530

    commit2

--- modal/index.js
+++ modal/index.js
@@ -2,15 +2,20 @@ $(document).ready(()=>{
 
 
 
-
-
-$(".button").click(function() {
-    $("#modl").css("display","flex");
+$(".button").click(function(){
+	$("#modl").css("display","flex");
+	$(".container").css("background","#808080")
 })
+
+
+
 
 $(".close").click(function() {
     $("#modl").css("display","none");
+    $(".container").css("background-color","#FFFFFF")
 })
+
+
   
 
 
